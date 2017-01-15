9d8c033452a9be65b9ab5a2270ef59c283639894
Łukasz Ozimek <lukaszozimek1@gmail.com>
2017-01-15 11:05:34 +0100

    Remove file which should be ingnored

--- src/main/java/io/protone/web/rest/CORAssociationResource.java
+++ src/main/java/io/protone/web/rest/CORAssociationResource.java
@@ -2,15 +2,12 @@ package io.protone.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 import io.protone.domain.CORAssociation;
-
 import io.protone.repository.CORAssociationRepository;
-import io.protone.web.rest.util.HeaderUtil;
 import io.protone.service.dto.CORAssociationDTO;
 import io.protone.service.mapper.CORAssociationMapper;
-
+import io.protone.web.rest.util.HeaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,10 +16,8 @@ import javax.inject.Inject;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * REST controller for managing CORAssociation.
