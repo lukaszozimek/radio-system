09b4c0e83e64e2c4ea463e10119ae941e32bd51d
Łukasz Ozimek <lukaszozimek1@gmail.com>
2017-01-21 12:52:59 +0100

    Provide Injection of service layer

--- src/main/java/io/protone/custom/web/rest/network/traffic/impl/ApiNetworkTrafficCampaignImpl.java
+++ src/main/java/io/protone/custom/web/rest/network/traffic/impl/ApiNetworkTrafficCampaignImpl.java
@@ -1,5 +1,6 @@
 package io.protone.custom.web.rest.network.traffic.impl;
 
+import io.protone.custom.service.TRACampaignService;
 import io.protone.custom.service.dto.TraCampaignPT;
 import io.protone.custom.web.rest.network.traffic.ApiNetworkTrafficCampaign;
 import io.swagger.annotations.ApiParam;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 import java.util.List;
 
 @RestController
-public class ApiNetworkTrafficCampaignImpl implements ApiNetworkTrafficCampaign{
-
+public class ApiNetworkTrafficCampaignImpl implements ApiNetworkTrafficCampaign {
+    @Inject
+    private TRACampaignService campaignService;
 
     @Override
     public ResponseEntity<List<TraCampaignPT>> getAllCampaignsUsingGET(@ApiParam(value = "networkShortcut", required = true) @PathVariable("networkShortcut") String networkShortcut) {
--- src/main/java/io/protone/custom/web/rest/network/traffic/impl/ApiNetworkTrafficCustomerOrderImpl.java
+++ src/main/java/io/protone/custom/web/rest/network/traffic/impl/ApiNetworkTrafficCustomerOrderImpl.java
@@ -1,5 +1,6 @@
 package io.protone.custom.web.rest.network.traffic.impl;
 
+import io.protone.custom.service.TRAOrderService;
 import io.protone.custom.service.dto.TraOrderPT;
 import io.protone.custom.web.rest.network.traffic.ApiNetworkTrafficCustomerOrder;
 import io.swagger.annotations.ApiParam;
@@ -7,10 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 import java.util.List;
 
 @RestController
 public class ApiNetworkTrafficCustomerOrderImpl implements ApiNetworkTrafficCustomerOrder {
+    @Inject
+    private TRAOrderService orderService;
 
     @Override
     public ResponseEntity<List<TraOrderPT>> getAllCustomerOrdersUsingGET(@ApiParam(value = "networkShortcut", required = true) @PathVariable("networkShortcut") String networkShortcut, @ApiParam(value = "customerShortcut", required = true) @PathVariable("customerShortcut") String customerShortcut) {
