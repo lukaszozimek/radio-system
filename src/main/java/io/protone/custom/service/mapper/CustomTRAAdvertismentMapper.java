47d74a984c79e952feda92e258de11852706b83a
Łukasz Ozimek <lukaszozimek1@gmail.com>
2017-01-21 20:02:33 +0100

    Provide Implemnetation for endpoint layer and start providing mappings

--- src/main/java/io/protone/custom/service/mapper/CustomTRAAdvertismentMapper.java
+++ src/main/java/io/protone/custom/service/mapper/CustomTRAAdvertismentMapper.java
@@ -1,10 +1,40 @@
 package io.protone.custom.service.mapper;
 
+import io.protone.custom.service.dto.LibItemPT;
+import io.protone.custom.service.dto.TraAdvertisementPT;
+import io.protone.custom.service.dto.TraCustomerPT;
+import io.protone.domain.LIBMediaItem;
+import io.protone.domain.TRAAdvertisement;
+import io.protone.domain.TRACustomer;
 import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
 
 /**
  * Created by lukaszozimek on 21.01.2017.
  */
 @Service
 public class CustomTRAAdvertismentMapper {
+    @Inject
+    private CustomTRAIndustryMapper customTRAIndustryMapper;
+
+    public TRAAdvertisement transformDTOToEntity(TraAdvertisementPT traAdvertisementPT) {
+        TRAAdvertisement advertisement = new TRAAdvertisement();
+        return advertisement.name(traAdvertisementPT.getName())
+            .description(traAdvertisementPT.getDescription())
+            .industry(customTRAIndustryMapper.tRAIndustryDTOToTRAIndustry(traAdvertisementPT.getIndustryId()))
+            .mediaItem(new LIBMediaItem())
+            .customer(new TRACustomer());
+    }
+
+    public TraAdvertisementPT transformEntityToDTO(TRAAdvertisement traAdvertisement) {
+        return new TraAdvertisementPT()
+            .id(traAdvertisement.getId())
+            .name(traAdvertisement.getName())
+            .description(traAdvertisement.getDescription())
+            .industryId(customTRAIndustryMapper.tRAIndustryToTRAIndustryDTO(traAdvertisement.getIndustry()))
+            .mediaItemId(new LibItemPT())
+            .customerId(new TraCustomerPT());
+    }
+
 }
--- src/main/java/io/protone/custom/service/mapper/CustomTRACustomerMapper.java
+++ src/main/java/io/protone/custom/service/mapper/CustomTRACustomerMapper.java
@@ -4,15 +4,22 @@ import io.protone.custom.service.dto.TraCustomerPT;
 import io.protone.domain.TRACustomer;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+
 /**
  * Created by lukaszozimek on 21.01.2017.
  */
 @Service
 public class CustomTRACustomerMapper {
 
+    @Inject
+    private CustomTRAIndustryMapper customTRAIndustryMapper;
+
     public TRACustomer transformDTOtoEntity(TraCustomerPT traCustomerPT) {
         TRACustomer traCustomer = new TRACustomer();
-        return traCustomer;
+        traCustomer.setId(traCustomerPT.getId());
+        return traCustomer.name(traCustomerPT.getName())
+            .industry(customTRAIndustryMapper.tRAIndustryDTOToTRAIndustry(traCustomerPT.getIndustry()));
     }
 
     public TraCustomerPT transformEntityToDTO(TRACustomer traCustomerPT) {
