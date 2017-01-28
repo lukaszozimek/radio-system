db317263bd269128ab439486b8555029e9f6df30
Łukasz Ozimek <lukaszozimek1@gmail.com>
2017-01-28 20:58:02 +0100

    Provide Structs for Metadata

---
+++ src/main/java/io/protone/custom/metadata/cartchunk/CartTimer.java
@@ -0,0 +1,25 @@
+package io.protone.custom.metadata.cartchunk;
+
+/**
+ * Created by lukaszozimek on 28.01.2017.
+ */
+public class CartTimer {
+    private String dwUsage;
+    private long dwValue;
+
+    public String getDwUsage() {
+        return dwUsage;
+    }
+
+    public void setDwUsage(String dwUsage) {
+        this.dwUsage = dwUsage;
+    }
+
+    public long getDwValue() {
+        return dwValue;
+    }
+
+    public void setDwValue(long dwValue) {
+        this.dwValue = dwValue;
+    }
+}
