72a09856d8e0bc4ab26caeae1b4dd3d681778ef3
lukaszozimek <lukaszozimek1@gmail.com>
2017-02-20 19:20:05 +0100

    Test Jenkins

--- mvnw.cmd
+++ mvnw.cmd
@@ -22,7 +22,7 @@
 @REM
 @REM Required ENV vars:
 @REM JAVA_HOME - location of a JDK home dir
-@REM
+@REM test
 @REM Optional ENV vars
 @REM M2_HOME - location of maven2's installed home dir
 @REM MAVEN_BATCH_ECHO - set to 'on' to enable the echoing of the batch commands
