4f9b6605b1ada6978a70648db1d9c0784a26974b
Łukasz Ozimek <lukaszozimek1@gmail.com>
2017-05-05 13:54:12 +0200

    Provide RestClasses which should be implemented

---
+++ src/test/java/io/protone/custom/web/rest/network/UserJWTControllerTest.java
@@ -0,0 +1,15 @@
+package io.protone.custom.web.rest.network;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lukaszozimek on 05.05.2017.
+ */
+public class UserJWTControllerTest {
+    @Test
+    public void authorize() throws Exception {
+    }
+
+}
---
+++ src/test/java/io/protone/custom/web/rest/network/library/ApiNetworkLibraryItemTest.java
@@ -0,0 +1,35 @@
+package io.protone.custom.web.rest.network.library;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lukaszozimek on 05.05.2017.
+ */
+public class ApiNetworkLibraryItemTest {
+    @Test
+    public void updateItemByNetworShortcutAndLibraryPrefixUsingPUT() throws Exception {
+    }
+
+    @Test
+    public void getAllItemsByNetworShortcutAndLibraryPrefixUsingGET() throws Exception {
+    }
+
+    @Test
+    public void uploadItemsByNetworShortcutAndLibraryPrefix() throws Exception {
+    }
+
+    @Test
+    public void getItemByNetworShortcutAndLibrarUsingGET() throws Exception {
+    }
+
+    @Test
+    public void deleteItemByNetworShortcutAndLibrarUsingDELETE() throws Exception {
+    }
+
+    @Test
+    public void streamItemByNetworShortcutAndLibrarUsingGET() throws Exception {
+    }
+
+}
