3d856e4eef0d07295b454559c614a54923f56d20
Łukasz Ozimek <lukaszozimek1@gmail.com>
2017-05-24 17:55:01 +0200

    [PROT-348] Migration Traffic Scheduling mechanism

--- src/main/java/io/protone/service/traffic/TraAdvertisementShuffleService.java
+++ src/main/java/io/protone/service/traffic/TraAdvertisementShuffleService.java
@@ -1,5 +1,8 @@
 package io.protone.service.traffic;
 
+import io.protone.domain.TraAdvertisement;
+import io.protone.domain.TraBlock;
+import io.protone.domain.TraEmission;
 import io.protone.web.rest.dto.traffic.TraShuffleAdvertisementDTO;
 import io.protone.domain.TraPlaylist;
 import org.slf4j.Logger;
@@ -7,7 +10,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by lukaszozimek on 06/03/2017.
@@ -28,31 +35,33 @@ public class TraAdvertisementShuffleService {
     public List<TraPlaylist> shuffleCommercials(TraShuffleAdvertisementDTO tarShuffleAdvertisementPT, String networkShortcut, String channelShortcut) {
         log.debug("Start shuffling commercial");
         log.debug("Commercial to shuffle {}", tarShuffleAdvertisementPT.getNumber());
-        //   TraAdvertisement traAdvertisement = traAdvertisementService.getAdvertisement(tarShuffleAdvertisementPT.getTraAdvertisementDTO().getId());
-        //   log.debug("Found advertisment {}", traAdvertisement.getId());
-        //   List<SchBlock> schBlockList = traPlaylistService.findByScheduledStartTimeBetweenAndType(tarShuffleAdvertisementPT.getFrom(), tarShuffleAdvertisementPT.getTo(), SchBlockTypeEnum.BT_COMMERCIAL);
-        //  log.debug("Found number of blocks in range : {}", schBlockList.size());
+        TraAdvertisement traAdvertisement = traAdvertisementService.getAdvertisement(tarShuffleAdvertisementPT.getTraAdvertisementDTO().getId(), networkShortcut);
+        log.debug("Found advertisment {}", traAdvertisement.getId());
+        List<TraPlaylist> traPlaylistListInRange = traPlaylistService.getTraPlaylistListInRange(tarShuffleAdvertisementPT.getFrom(), tarShuffleAdvertisementPT.getTo(), networkShortcut, channelShortcut);
+        log.debug("Found number of Playlist in range : {}", traPlaylistListInRange.size());
         int numberOfCommercialsShuffled = 0;
         int numberOfScheduledBlocks = 0;// = schBlockList.size();
         for (int i = 0; i < numberOfScheduledBlocks; i++) {
+            TraPlaylist traPlaylist = traPlaylistListInRange.get(i);
             if (numberOfCommercialsShuffled != tarShuffleAdvertisementPT.getNumber()) {
-                //            int blockIndex = java.util.concurrent.ThreadLocalRandom.current().nextInt(numberOfScheduledBlocks);
-                //          log.debug("Check is it possible to shuffle commercial in block: {}", blockIndex);
-                //*   List<SchEmission> schEmissionList = schEmissionRepository.findByBlock(schBlockList.get(blockIndex));
-              /*   long numberOfAdvertisements = schEmissionList.stream()
-                    .filter(schEmission ->
-                        schEmission.getMediaItem().getIdx().equalsIgnoreCase(tarShuffleAdvertisementPT.getTraAdvertisementDTO().getMediaItemId().getIdx())).count();*/
+                int blockIndex = java.util.concurrent.ThreadLocalRandom.current().nextInt(numberOfScheduledBlocks);
+                log.debug("Check is it possible to shuffle commercial in block: {}", blockIndex);
+                TraBlock traBlock = traPlaylist.getPlaylists().stream().collect(toList()).get(blockIndex);
+                Set<TraEmission> traEmissionSet = traBlock.getEmissions();
+                long numberOfAdvertisements = traEmissionSet.stream().filter(traEmission -> traEmission.getAdvertiment().getMediaItem().getIdx().equalsIgnoreCase(tarShuffleAdvertisementPT.getTraAdvertisementDTO().getMediaItemId().getIdx())).count();
                 ///Add Filtering by lenghtScheduledTime
-               /*  if (numberOfAdvertisements == 0) {
-           //         SchBlock schBlock = schBlockList.get(blockIndex);
-                    //     SchEmission emission = new SchEmission().block(schBlock).seq(schEmissionList.size() + 1).mediaItem(libItemMapper.DTO2DB(tarShuffleAdvertisementPT.getTraAdvertisementDTO().getMediaItemId()));
-                    numberOfCommercialsShuffled++;*/
-
-            } else {
-                break;
+                if (numberOfAdvertisements == 0) {
+                    Long lastTimeStop = traBlock.getEmissions().stream().max(Comparator.comparingLong(TraEmission::getTimeStop)).get().getTimeStop();
+                    if (lastTimeStop < traBlock.getLength() && (lastTimeStop + traAdvertisement.getMediaItem().getLength().longValue()) <= traBlock.getLength()) {
+                        TraEmission emisssion = new TraEmission().block(traBlock).timeStart(lastTimeStop).timeStop(lastTimeStop + traAdvertisement.getMediaItem().getLength().longValue()).advertiment(traAdvertisement);
+                        numberOfCommercialsShuffled++;
+                    }
+                } else {
+                    break;
+                }
             }
+            log.debug("Number shuffled: {}, Number unshuffled commercials : {}", numberOfCommercialsShuffled, tarShuffleAdvertisementPT.getNumber() - numberOfCommercialsShuffled);
         }
-        log.debug("Number shuffled: {}, Number unshuffled commercials : {}", numberOfCommercialsShuffled, tarShuffleAdvertisementPT.getNumber() - numberOfCommercialsShuffled);
-        return null;
+        return traPlaylistListInRange;
     }
 }
